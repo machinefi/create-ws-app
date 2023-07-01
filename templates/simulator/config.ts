1d496c2af3b2bb4639701a7c7950a4932bb28d83
Simon <44496148+siim0n@users.noreply.github.com>
2023-07-01 15:51:30 +0200

    Update config.ts
    The url apparently changed, last one gave ETIMEDOUT

--- templates/simulator/config.ts
+++ templates/simulator/config.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 export default {
   PUB_TOKEN: process.env.PUB_TOKEN || "",
-  W3BSTREAM_ENDPOINT: `http://dev.w3bstream.com:8889/srv-applet-mgr/v0/event/${
+  W3BSTREAM_ENDPOINT: `https://devnet-prod.w3bstream.com/api/w3bapp/event/${
     process.env.PROJECT_NAME
   }?eventType=${process.env.EVENT_TYPE || "DEFAULT"}`,
 };
