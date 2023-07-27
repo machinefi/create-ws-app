9a86413389281ab15cbebbdc5724d3b5795a7031
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-07-27 12:31:43 -0300

    update tasks

--- templates/blockchain/tasks/welcome.ts
+++ templates/blockchain/tasks/welcome.ts
@@ -1,3 +1,5 @@
+import { task } from "hardhat/config";
+
 task("hello-world", "Prints hello world").setAction(async (taskArgs, hre) => {
   console.log("Hello World!");
 });
---
+++ templates/extensions/erc1155/tasks/rewards.ts
@@ -0,0 +1,39 @@
+import { task } from "hardhat/config";
+
+task(
+  "grant-rewards-minter",
+  "Grant rewards minter role to an address"
+).setAction(async (_, hre) => {
+  const { deployments } = hre;
+  const [deployer] = await hre.ethers.getSigners();
+
+  const DeviceRewards = await deployments.get("DeviceRewards");
+  const rewards = await hre.ethers.getContractAt(
+    "DeviceRewards",
+    DeviceRewards.address,
+    deployer
+  );
+
+  const minterRole = await rewards.MINTER_ROLE();
+  const tx = await rewards.grantRole(minterRole, process.env.OPERATOR_ADDRESS);
+  await tx.wait();
+
+  console.log(`Minter role granted to ${process.env.OPERATOR_ADDRESS}`);
+});
+
+task("update-rewards-uri", "Update rewards uri").setAction(async (_, hre) => {
+  const { deployments } = hre;
+  const [deployer] = await hre.ethers.getSigners();
+
+  const DeviceRewards = await deployments.get("DeviceRewards");
+  const rewards = await hre.ethers.getContractAt(
+    "DeviceRewards",
+    DeviceRewards.address,
+    deployer
+  );
+
+  const tx = await rewards.setURI(process.env.REWARDS_URI || "");
+  await tx.wait();
+
+  console.log(`DeviceRewards uri updated to ${process.env.REWARDS_URI}`);
+});
