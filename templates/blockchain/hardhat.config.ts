0b7d945d74c049fbcdc58299c353ef412227c082
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-07-27 12:23:18 -0300

    add typescript config

---
+++ templates/blockchain/hardhat.config.ts
@@ -0,0 +1,28 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy";
+import "dotenv/config";
+
+import "./tasks";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    testnet: {
+      url: "https://babel-api.testnet.iotex.io",
+      accounts: [process.env.IOTEX_PRIVATE_KEY || ""],
+    },
+    mainnet: {
+      url: "https://babel-api.mainnet.iotex.io",
+      accounts: [process.env.IOTEX_PRIVATE_KEY || ""],
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+};
+
+export default config;
